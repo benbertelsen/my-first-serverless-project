f5f9100fbd7060250d741c93812877c445270552
Benjamin Bertelsen <benjaminbertelsen@gmail.com>
2021-08-10 23:53:21 +0200

    final commit

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,17 +1,25 @@
-//tell program we want to run node fetch as installed with NPM
+//Our serverless function runs here and calls the API without the need for a server-side code workspace
 
+//tell program we want to run node fetch as installed with NPM
 const fetch = require('node-fetch')
 
-exports.handler = async function () {
-    //Defining the RUL
-    const POKE_API = 'https://pokeapi.co/api/v2/pokemon/charmander'
+exports.handler = async function (event, context) {
+    const eventBody = JSON.parse(event.body)
+    //Defining the API, and making it able to shift between Kanto og Hoenn with string interpolation (just like ${ ... })
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
+
     
+console.log(event)
+
     //define a response
     const response = await fetch(POKE_API)
     const data = await response.json()
 
+    //Asking just to fetch for somethign specific in the API (the entries)
     return {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+            pokemon: data.pokemon_entries
+        })
     }
 }
